18a859e472baa60a62e657470da8cd2d249425c4
EvgeniyT <evgeniy.titov@kilo.solutions>
2026-03-23 18:33:54 +0100

    feature(User) added user entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import { ILoggerService } from '../logger/logger.interface.js';
 import { IUsersController } from './users.controller.interface.js';
 import { UserRegisterDto } from './dto/user-register.dto.js';
 import { UserLoginDto } from './dto/user-login.dto.js';
+import { User } from './user.entity.js';
 
 @injectable()
 export class UsersController extends BaseController implements IUsersController {
@@ -27,8 +28,10 @@ export class UsersController extends BaseController implements IUsersController 
 		// next(new HttpError(400, 'Login error', 'Login'));
 	}
 
-	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction) {
-		console.log(req.body);
-		return this.ok(res, 'Register');
+	async register({ body }: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction) {
+		console.log(body);
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
+		return this.ok(res, newUser);
 	}
 }
